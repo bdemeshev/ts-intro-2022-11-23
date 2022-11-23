46fb4f13038f3951fa9c415628c48f1fdcdf65b4
bdemeshev <boris.demeshev@gmail.com>
2022-11-23 17:16:42 +0300

    zero.R

---
+++ zero.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(rio)
+library(fpp3)
+
+url_orig = 'https://github.com/bdemeshev/webinar_eusp_forecasting_r_2021_03_13/raw/main/original_data.xls'
+url_prep = 'https://github.com/bdemeshev/webinar_eusp_forecasting_r_2021_03_13/raw/main/monthly_marriages_2020.csv'
+
+# source:
+# https://fedstat.ru/indicator/33553
